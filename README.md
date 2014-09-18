8ab873240adfb2a816a3337c15b41a918cc8dfa6
Colin Megill <colinmegill@gmail.com>
2014-09-18 00:21:57 -1000

    transitioning to tracks, rather than to...

---
+++ README.md
@@ -0,0 +1,3 @@
+g0v conference 2014, see below url for content:
+
+https://docs.google.com/spreadsheets/d/14D-Vt-iysQ3Czqj8IN-ov6krvQlC06ny-6CxF49hsCk/pubhtml?gid=726275408&single=true
---
+++ js/data.js
@@ -0,0 +1,37 @@
+var tracks = [
+	
+	{
+		title: "Open Parliament",
+		description: "Using open information to supervise representative government is an important aspect of the open government movement. Every country has undergone a different journey of development. The g0v community has also developed long-standing applications in this regard. \n This track includes the participation of open parliament activists from many countries as well as senior promoters of early parliamentary supervision in Taiwan. Through multiple perspectives, they will explore new possibilities for digital citizens and representative government."
+		talks: [{ title: "Interest Inspector", speaker: "Felipe Álvarez" org: "FCI" }, {title: "Using Open Data to Empower Others", speaker: "Matthew Rumsey", org: "Sunlight Foundation" }]
+	},
+	{
+		title: "Participatory Democracy",
+		description: "How does one participate in democracy and how to make participatory democracy possible have become issues of growing interest in recent years. At its inception, the g0v community had already discussed the related cases and set out to work on the platform development. However, more efforts are still needed in various areas in open data and infrastructures for the deepening of the democratization process. \n In this track, we have invited two international speakers to introduce Europe's Pirate Party and their online platform review experience. The panel will involve a direct dialogue between executive committee members who have promoted openness and participation in the cabinet with academics who have advocated deliberate democracy in the community"
+	},
+	{
+		title: "Reclaiming our language",
+		description: "This Moedict project stems from the Ministry of Education's revision of the Mandarin Dictionary. With assistance from various quarters, it gradually integrates Taiwan's various languages and facilitates a multicultural dialogue in the spirit of open information. \n We are honored to have enlisted the help of the Ministry of Education and the pioneers of Taiwanese languages digitization in the education sector and have them share with us the fruit of our success in recent years. The new design for Moedict's website and mobile app will be unveiled for the first time during the summit."
+	},
+	{
+		title: "Civic Technologies",
+		description: "Data visualization, data journalism, encrypted communications systems and network analysis: These information technologies have initiated new modes of participation and colllaboration for citizens. \n In what ways can we integrate the issues through new analysis and application, in order to have a greater impact on policy planning, media forms and in-depth information interpretation and analysis? A number of hackers will share their exciting practical work with us."
+	},
+	{
+		title: "Student activism under the spotlight",
+		description: "The functions of the media have changed some of the past campaign practices of the Sunflower Student Movement. The interactive functions between mainstream media and new media from television, newspapers, magazines to online space, social media and the decentralized open-source online community have all undergone new forms of experimentation. \n This panel will discuss whether the role of the media — as a participant vs. an intervenor of the protest movement. We will also discuss the role of the online community as well as the prospects for the future."
+	},
+	{
+		title: "Open data and the environment",
+		description: "Presently, climate change and the overuse of environmental resources have caused many natural disasters to occur. The rain/temperature gauge and air pollution map created by g0v have helped to bring the general public closer to environmental information. \n With this experience, we have used the opportunity offered by the g0v summit to invite three experts respectively from the industry, NGO and government to discuss the global and local (Taiwan) environmental issues, surveillance and data and any new meteorological events."
+	},
+	{
+		title: "Age of Collaboration",
+		description: "The rise of social networking sites made decentralized activism practical; everything from simple information delivery to global collaborative work has been made possible because of the rise of technology and crowd participation. \n How would communication technologies — combined with crowd-sourcing — accelerate the process of social change? This track features transnational citizen technology communities, the large-scale g0v-mobilized crowd OCR projects, as well as the two cornerstones of open content: 'Wikipedia' and 'OpenStreetMap', exploring the new driving forces of collaboration through practical case studies."
+	},
+	{
+		title: "New faces of activism",
+		description: "What sparks are set off when social activists cross paths with technologies? New methods of dissemination, new tools of collaboration and new forms of communication are all influential on many levels. The processes of mutual communicating, learning and influencing all plays a part in the progress of a civil society. \nThis discussion about the new faces of activism will start with the new media in the Sunflower Student Movement, then move on to explore the possibility of combining technological tools and social activism, including how Hong Kong activists leveraged experiences and software from Taiwan's civic hackers."
+	}
+
+];
